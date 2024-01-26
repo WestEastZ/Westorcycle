62d1ae353e46576539ef44c07a3f5496be6e61fe
WestEastZ <tjehdwn10110@naver.com>
2024-01-26 16:29:10 +0900

    ✨ 로그아웃 기능 초기 구현

--- src/page/ConsumerProfile.tsx
+++ src/page/ConsumerProfile.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { auth } from "@/firebase";
+import { signOut } from "firebase/auth";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 
 export default function ConsumerProfile() {
-  return <div>구매자 전용입니다.</div>;
+  const navigate = useNavigate();
+  const Logout: React.MouseEventHandler<HTMLButtonElement> = async (event) => {
+    event.preventDefault();
+    localStorage.removeItem("user");
+    await signOut(auth);
+    navigate("/");
+  };
+  return (
+    <>
+      <div>구매자 전용입니다.</div>
+      <Button onClick={Logout}>로그아웃</Button>
+    </>
+  );
 }
