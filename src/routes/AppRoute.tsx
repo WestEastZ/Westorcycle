a77e4ed03320a7dc5226283c0e6ff398fa142ccc
WestEastZ <tjehdwn10110@naver.com>
2024-01-27 01:20:39 +0900

    ✨ contextAPI 로그인 정보에 따른 라우트 분기 처리, 로그인, 로그아웃 기능 수정

--- src/routes/AppRoute.tsx
+++ src/routes/AppRoute.tsx
@@ -1,17 +1,59 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ProtectedRoute } from "./ProtectedRoutes";
 
 import Home from "@/page/Home";
 import Login from "@/page/Login";
 import SignUp from "@/page/SignUp";
+import { useGetUser } from "@/contexts/userContext";
+import SellerProfile from "@/page/SellerProfile";
+import ConsumerProfile from "@/page/ConsumerProfile";
 
 export default function AppRoute() {
+  const user = useGetUser();
+
   return (
     <BrowserRouter>
       <Routes>
+        {/* 모든 사용자 방문 가능 */}
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+
+        {/* 로그인 유무 ? 홈 : children */}
+        <Route
+          path="/login"
+          element={
+            <ProtectedRoute condition={!user}>
+              <Login />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/signup"
+          element={
+            <ProtectedRoute condition={!user}>
+              <SignUp />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* 로그인 유무 && 판매자 여부 ? 홈 : children  */}
+        <Route
+          path="/seller"
+          element={
+            <ProtectedRoute condition={user && user.isSeller}>
+              <SellerProfile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/consumer/"
+          element={
+            <ProtectedRoute condition={user && !user.isSeller}>
+              <ConsumerProfile />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
