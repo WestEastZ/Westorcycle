23684d14bf28c95999f9570f3d6065fc52fd95cc
WestEastZ <tjehdwn10110@naver.com>
2024-02-02 22:47:09 +0900

    ✨ 상품 카테고리 페이지 제작 / 카테고리 별 제품 불러오기 및 세부 필터 기능 구현

--- src/firebase/fetch/fetchInfinityProduct.tsx
+++ src/firebase/fetch/fetchInfinityProduct.tsx
@@ -2,6 +2,7 @@ import { db } from "@/firebase";
 import { Product, ProductWithId, UserType } from "@/models/type";
 import {
   DocumentSnapshot,
+  OrderByDirection,
   collection,
   getDocs,
   limit,
@@ -20,30 +21,37 @@ export default async function fetchInfinityProduct({
   queryKey: unknown[];
 }) {
   // queryKey 변수 선언
-  const [_key, user, params, category, option] = queryKey as [
-    string,
-    string,
-    string,
-    string,
-    string
-  ];
+  const [_key, user, params, category, option, direction = "desc"] =
+    queryKey as [string, string, string, string, string, OrderByDirection];
 
   // 실행
   try {
-    let q = query(collection(db, _key), limit(4));
+    let q = query(collection(db, _key), limit(2));
 
     // 판매자 본인 확인
     if (user == params) {
       q = query(q, where("sellerId", "==", user));
     }
+
     // 카테고리
     if (category) {
       q = query(q, where("productCategory", "==", category));
     }
 
     // 정렬 조건
-    if (option == "") {
-      q = query(q, orderBy("updatedAt", "desc"));
+    switch (option) {
+      case "price":
+        q = query(q, orderBy("productPrice", direction));
+        break;
+      // case "likes": -> 색인 생성 완료 추후 사용
+      //   q = query(q, orderBy("likes", direction));
+      //   break;
+      case "updatedAt":
+        q = query(q, orderBy("updatedAt", direction));
+        break;
+      default:
+        q = query(q, orderBy("updatedAt", "desc"));
+        break;
     }
 
     // 페이지 시작 지점
@@ -58,7 +66,7 @@ export default async function fetchInfinityProduct({
       const product = doc.data() as Product;
       return { docId: doc.id, ...product } as ProductWithId;
     });
-
+    console.log(data);
     return { data, lastVisible };
   } catch (error) {
     console.log(error);
--- src/page/Seller/SellerProfile.tsx
+++ src/page/Seller/SellerProfile.tsx
@@ -9,18 +9,21 @@ import fetchInfinityProduct from "@/firebase/fetch/fetchInfinityProduct";
 import { useInView } from "react-intersection-observer";
 import { ProductWithId } from "@/models/type";
 import ProductCard from "../../components/card/ProductCard";
+import { OrderByDirection } from "firebase/firestore";
 
 export default function SellerProfile() {
   const user = useUser();
   const params = useParams();
+  const paramsId = params.id;
   const { ref, inView } = useInView();
 
   const [category, setCategory] = useState<string>("");
   const [option, setOption] = useState<string>("");
+  const [direction, setDirection] = useState<OrderByDirection>("desc");
 
   // react-query
   const { data, hasNextPage, fetchNextPage, isFetching } = useInfiniteQuery(
-    ["product", user?.id, params.id, category, option],
+    ["product", user?.id, paramsId, category, option, direction],
     ({ pageParam, queryKey }) => fetchInfinityProduct({ pageParam, queryKey }),
     {
       getNextPageParam: (lastPage) => lastPage?.lastVisible,
