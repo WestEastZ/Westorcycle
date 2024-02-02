03dea47975eb4e6522e910488bc003625e19aa3f
WestEastZ <tjehdwn10110@naver.com>
2024-02-03 03:34:23 +0900

    💄 홈 / 로그인 / 회원가입 스타일링

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import { UserProvider } from "./contexts/userContext";
 import "./App.css";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-import { Reset } from "styled-reset";
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <UserProvider>
-        <Reset />
         <AppRoute />
       </UserProvider>
       <ReactQueryDevtools />
