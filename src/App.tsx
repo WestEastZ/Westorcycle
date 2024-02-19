6c45acccd73005a7625c7c866390b66bb614e137
WestEastZ <tjehdwn10110@naver.com>
2024-02-19 22:17:42 +0900

    ⚡️ react-helmet-async 변경

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import { UserProvider } from "./contexts/userContext";
 import "./App.css";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import { HelmetProvider } from "react-helmet-async";
 
 function App() {
   const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
-      <UserProvider>
-        <AppRoute />
-      </UserProvider>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <UserProvider>
+          <AppRoute />
+        </UserProvider>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </HelmetProvider>
   );
 }
 
