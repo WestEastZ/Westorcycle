fe77f7345ce3bd34d8b080fcb368de9f88172eb3
WestEastZ <tjehdwn10110@naver.com>
2024-02-26 16:51:02 +0900

    🐛 og태그 적용

--- src/utils/SEOHelmet.tsx
+++ src/utils/SEOHelmet.tsx
@@ -15,11 +15,10 @@ export default function SEOHelmet({
 }: SEOHelmetProps) {
   return (
     <Helmet>
-      <meta charSet="utf-8"></meta>
       <title>{title} | WESTORCYCLE</title>
       <meta name="description" content={description} />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="robots" content="index,nofollow"></meta>
+
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={imageUrl} />
